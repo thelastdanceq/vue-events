837e921bd24f843c23a5623f3f12f4267ad2a617
Vladyslav <baskov228322@gmail.com>
2022-06-10 13:50:20 +0300

    fixes

--- src/components/CardDetails.vue
+++ src/components/CardDetails.vue
@@ -7,24 +7,24 @@
         </button>
         <div class="card-details-modal-info">
           <p class="card-details-modal-title">
-            Title : {{ events.filter((el) => el.id === $attrs.id)[0].title }}
+            Title : {{ targetEvent.title }}
           </p>
           <p class="card-details-modal-title">
             Category :
-            {{ events.filter((el) => el.id === $attrs.id)[0].category }}
+            {{ targetEvent.category }}
           </p>
           <p class="card-details-modal-title">
             Description :
-            {{ events.filter((el) => el.id === $attrs.id)[0].description }}
+            {{ targetEvent.description }}
           </p>
           <p class="card-details-modal-title">
             Organizer :
             {{
-              events.filter((el) => el.id === $attrs.id)[0].organizer instanceof
-              Object
-                ? events.filter((el) => el.id === $attrs.id)[0].organizer.user
+                targetEvent.organizer instanceof
+                  Object
+                  ? targetEvent.organizer.user
                     .name
-                : events.filter((el) => el.id === $attrs.id)[0].organizer
+                  : targetEvent.organizer
             }}
           </p>
         </div>
@@ -35,11 +35,20 @@
 
 <script lang="ts">
 import router from "@/router";
+import { IEvent } from "@/types/types";
 import Vue from "vue";
 export default Vue.extend({
   components: {},
-  props: {
-    events: [],
+  props: ['events'],
+  data() {
+    return {
+      eventId: this.$route.params.id
+    }
+  },
+  computed: {
+    targetEvent(): IEvent {
+      return this.events.find((event: IEvent) => event.id === Number(this.eventId))
+    }
   },
   methods: {
     handleClose() {
@@ -68,11 +77,13 @@ export default Vue.extend({
     height: 700px;
     position: relative;
     background-color: white;
+
     & .close-btn {
       position: absolute;
       top: 1rem;
       right: 1rem;
     }
+
     &-info {
       font-size: 20px;
       position: absolute;
